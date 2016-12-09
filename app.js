a74c22a4fe40425db27ac3fd88a701eacab73a5a
jhta <jeisonhs93@gmail.com>
2016-12-09 14:25:48 -0500

    [update] node app

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var logger = require('morgan');
 var path = require('path');
-var api = require('./api');
+var api = require('./server/api');
 var app = express();
-
+var SERVER = path.join(__dirname, 'server')
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(SERVER, 'public')));
 app.use('/api', api);
 
 module.exports = app;
