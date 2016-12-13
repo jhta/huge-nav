76f726f6ba7bfc233fbcbd4c8c2c8a4c29175105
jhta <jeisonhs93@gmail.com>
2016-12-13 17:42:11 -0500

    [add] ES6 loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,46 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+// Stylus mixins
 const rupture = require('rupture');
 const nib = require('nib');
 
 module.exports = {
+
   entry: {
     app: './src/index.js',
-    //about: './src/about.js',
+    render: './src/js/render.js',
   },
   output: {
     path: './server/public/',
     filename: '[name].js',
   },
   devtool: "source-map",
+
   module: {
     loaders: [
+      // load for babel ES6
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        exclude: /(node_modules|public)/,
+      },
+      //Loader for stylus
       {
         test: /\.styl$/,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
       },
-      // Font Definitions
+      // Loader for fonts
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'
       },
+      //loader for images
       {
         test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'
       }
     ]
-   },
+  },
+
    stylus: {
     use: [
       rupture(),
@@ -36,8 +48,9 @@ module.exports = {
     ],
     compress: true,
    },
+
    plugins: [
      new ExtractTextPlugin('[name].css'),
-     //new webpack.optimize.CommonsChunkPlugin('commons', 'common.js')
+     new webpack.optimize.UglifyJsPlugin()
    ],
 };
